d224ebc57c6b873c6b15a1182de2958f1cd15c39
Kazeem Mariam Abosede <kazeemmariam16@gmail.com>
2024-11-07 16:53:56 +0100

    Add files via upload

---
+++ #The band name generator works by combin.py
@@ -0,0 +1,7 @@
+#The band name generator works by combining the user city and favorite pet 
+print("Welcome to Band Name Generator")
+print("This band generator works by combining the name of your city and favorite pet")
+city = input("What is the name of the city you grew up in?\n")
+Pet = input("What is the name of your pet?\n")
+city_pet = print("Your band name could be" + " " + city + " " + Pet)
+
